330ee0e1c2c6b757586c497c07cc91fb31cf70aa
jblack016 <j.black16@icloud.com>
2022-10-01 15:34:40 -0500

    Update DaikichiController.java

--- Spring/daikichiroutes/src/main/java/com/jaredblack/daikichiroutes/controllers/DaikichiController.java
+++ Spring/daikichiroutes/src/main/java/com/jaredblack/daikichiroutes/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.jaredblack.daikichiroutes.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,19 @@ public class DaikichiController {
 	@RequestMapping("/daikichi/tomorrow") // Default GET
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/daikichi/travel/{place}") // Default GET
+	public String travel(@PathVariable("place") String place) {
+		return "Congratulations! You will travel to " + place + " soon!";
+	}
+	
+	@RequestMapping("/daikichi/lotto/{num}") // Default GET
+	public String lotto(@PathVariable("num") int num) {
+		if(num % 2 == 0 ) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers!";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
