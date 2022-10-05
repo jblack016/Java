11aa72e587552d6a81fd40a59c659bb206c2ef29
jblack016 <j.black16@icloud.com>
2022-10-04 20:39:11 -0500

    Initial Commit

---
+++ Spring/counter/src/main/java/com/jaredblack/counter/controllers/HomeController.java
@@ -0,0 +1,62 @@
+package com.jaredblack.counter.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index(HttpSession session, Model model) {
+		if(session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		} else {
+			Integer currentCount = (Integer) session.getAttribute("count");
+			currentCount++;
+			session.setAttribute("count", currentCount);
+		}
+		return "welcomePage.jsp";
+	}
+	
+	
+	@RequestMapping("/counter/")
+	public String counter(HttpSession session, Model model, HttpServletRequest request) {
+		Integer currentCount = 0;
+		if(session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		} else {
+			currentCount = (Integer) session.getAttribute("count");
+		}
+//		Can use the following line to get the URL of our page, in this case http://localhost:8080/counter/
+		
+		String homeUrl = request.getRequestURL().toString();
+		model.addAttribute("count", currentCount);
+//		Removes the last nine characters from homeUrl to match the desired wireframe out
+//		http://localhost:8080 instead of http://localhost:8080/counter/
+		
+		model.addAttribute("page", homeUrl.substring(0,homeUrl.length()-9));
+		return "counterPage.jsp";
+	}
+	
+	@RequestMapping("/double-counter/")
+	public String doubleCounter(HttpSession session, Model model) {
+		if(session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		} else {
+			Integer currentCount = (Integer) session.getAttribute("count");
+			currentCount+=2;
+			session.setAttribute("count", currentCount);
+		}
+		return "doubleCounter.jsp";
+	}
+	
+	@RequestMapping("/reset-counter/")
+	public String resetCounter(HttpSession session, Model model, HttpServletRequest request) {
+		session.setAttribute("count", 0);
+		return "redirect:/counter/";
+	}
+}
---
+++ Spring/counter/src/main/webapp/WEB-INF/welcomePage.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="/webjars/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="/css/main.css"/>
+<meta charset="UTF-8">
+<title>Welcome Page</title>
+</head>
+<body>
+<h1 style="text-align: center;">Welcome User!</h1>
+<p style="text-align: center;"><a href="/counter/">Back to counter</a></p>
+</body>
+</html>
