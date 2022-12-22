7c73bf113c5c86210ddb91a79942b2d38d3804e7
jblack016 <j.black16@icloud.com>
2022-12-21 22:09:37 -0600

    Updated Commit

---
+++ Spring/eventplanner-1/src/main/java/com/jaredblack/eventplanner/Eventplanner1Application.java
@@ -0,0 +1,13 @@
+package com.jaredblack.eventplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eventplanner1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eventplanner1Application.class, args);
+	}
+
+}
